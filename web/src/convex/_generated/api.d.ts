016ef736b056c7989d82c8422ce9dcb1420ad018
Cédric PONTET <cpontet@agilepartner.net>
2025-08-09 01:58:36 +0200

    style: :art: commit generated code as expected

--- web/src/convex/_generated/api.d.ts
+++ web/src/convex/_generated/api.d.ts
@@ -8,21 +8,25 @@
  * @module
  */
 
-import type { ApiFromModules, FilterApi, FunctionReference } from 'convex/server';
-import type * as articles from '../articles.js';
-import type * as clearData from '../clearData.js';
-import type * as events from '../events.js';
-import type * as fileMigration from '../fileMigration.js';
-import type * as fixIdMappings from '../fixIdMappings.js';
-import type * as games from '../games.js';
-import type * as imageMigration from '../imageMigration.js';
-import type * as players from '../players.js';
-import type * as search from '../search.js';
-import type * as strapiMigration from '../strapiMigration.js';
-import type * as strapiMigrationBatched from '../strapiMigrationBatched.js';
-import type * as strapiMigrationQueries from '../strapiMigrationQueries.js';
-import type * as supporting from '../supporting.js';
-import type * as types from '../types.js';
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+import type * as articles from "../articles.js";
+import type * as clearData from "../clearData.js";
+import type * as events from "../events.js";
+import type * as fileMigration from "../fileMigration.js";
+import type * as fixIdMappings from "../fixIdMappings.js";
+import type * as games from "../games.js";
+import type * as imageMigration from "../imageMigration.js";
+import type * as players from "../players.js";
+import type * as search from "../search.js";
+import type * as strapiMigration from "../strapiMigration.js";
+import type * as strapiMigrationBatched from "../strapiMigrationBatched.js";
+import type * as strapiMigrationQueries from "../strapiMigrationQueries.js";
+import type * as supporting from "../supporting.js";
+import type * as types from "../types.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -48,5 +52,11 @@ declare const fullApi: ApiFromModules<{
   supporting: typeof supporting;
   types: typeof types;
 }>;
-export declare const api: FilterApi<typeof fullApi, FunctionReference<any, 'public'>>;
-export declare const internal: FilterApi<typeof fullApi, FunctionReference<any, 'internal'>>;
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
