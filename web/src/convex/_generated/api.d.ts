29f605f401ec8e7db150cb8317e6eecc2201cecf
Cédric PONTET <cpontet@agilepartner.net>
2025-08-12 18:25:36 +0200

    chore: :art: commit generated code

---
+++ web/src/convex/_generated/api.d.ts
@@ -0,0 +1,62 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+import type * as articles from "../articles.js";
+import type * as clearData from "../clearData.js";
+import type * as events from "../events.js";
+import type * as fileMigration from "../fileMigration.js";
+import type * as fixIdMappings from "../fixIdMappings.js";
+import type * as games from "../games.js";
+import type * as imageMigration from "../imageMigration.js";
+import type * as players from "../players.js";
+import type * as search from "../search.js";
+import type * as strapiMigration from "../strapiMigration.js";
+import type * as strapiMigrationBatched from "../strapiMigrationBatched.js";
+import type * as strapiMigrationQueries from "../strapiMigrationQueries.js";
+import type * as supporting from "../supporting.js";
+import type * as types from "../types.js";
+
+/**
+ * A utility for referencing Convex functions in your app's API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+declare const fullApi: ApiFromModules<{
+  articles: typeof articles;
+  clearData: typeof clearData;
+  events: typeof events;
+  fileMigration: typeof fileMigration;
+  fixIdMappings: typeof fixIdMappings;
+  games: typeof games;
+  imageMigration: typeof imageMigration;
+  players: typeof players;
+  search: typeof search;
+  strapiMigration: typeof strapiMigration;
+  strapiMigrationBatched: typeof strapiMigrationBatched;
+  strapiMigrationQueries: typeof strapiMigrationQueries;
+  supporting: typeof supporting;
+  types: typeof types;
+}>;
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
